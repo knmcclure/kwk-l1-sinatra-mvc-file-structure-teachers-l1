7d6bf449611aded1113965325f8ba905ee429479
Kyla McClure <kyla.mcclure.16@tfacorps.org>
2018-07-31 01:46:32 +0000

    automatically backed up by learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,22 @@
 class Model
-  # Replace with Dog clas
+  class Dog 
+    @@breed_count = []
+    def initiate(name, breed)
+      @name = name
+      @breed = breed
+    end
+    
+    def self.breed_count
+      breed_hash = {}
+      @@breed_count.each do |count|
+        if @@breed_count[count]
+          breed_hash +=1 
+        else @@breed_count[]
+          breed_hash = 1
+        end
+      end
+    end
+    
+  end
+    
 end
